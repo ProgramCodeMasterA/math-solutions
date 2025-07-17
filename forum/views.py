2186099980324e8aeca218a82f71f5f8cecc39cd
ProgramCodeMasterA <vb7zl582@students.codeinstitute.net>
2025-07-17 19:05:14 +0100

    Test Post model

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
-class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = 'post_list.html'
+def math_forum(request):
+    return HttpResponse("Hello, Forum")
